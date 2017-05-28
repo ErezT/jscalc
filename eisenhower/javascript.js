6267ed259f435e44960f1eff4e8d6764b5a37b44
ErezT <erezpitke@gmail.com>
2017-05-28 22:38:34 +0300

    add functions for add to array

--- eisenhower/javascript.js
+++ eisenhower/javascript.js
@@ -4,18 +4,35 @@ var tasksArray = [];
 var taskEntered = "";
 var array = document.getElementById('arr');
 var list = document.getElementById('list');
+
+
 function addToArray() {
 taskEntered = document.getElementById('task').value;
 tasksArray.push(taskEntered);
-console.log("task: " + taskEntered);
-console.log(tasksArray);
-createItem(taskEntered);
-// array.innerHTML = tasksArray;
+// console.log("task: " + taskEntered);
+// console.log(tasksArray);
+// createItem(taskEntered);
+refreshList();
 }
+
 
 function createItem(taskName) {
     var item = document.createElement("li");
     var nameOfItem = document.createTextNode(taskName);
     item.appendChild(nameOfItem);
     list.appendChild(item);
+    
+}
+
+function removeLastItem() {
+    tasksArray.pop();
+    list.removeLastItem;
+    console.log(tasksArray);
+    refreshList();
+}
+
+function refreshList() {
+    for(var i = 0; i < tasksArray.length; i++) {
+        createItem(tasksArray[i]);
+    }
 }
